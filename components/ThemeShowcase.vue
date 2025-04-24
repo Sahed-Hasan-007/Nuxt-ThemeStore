def0a63698649f2f301319a127718a3b562b5f47
p@ppu <skpappu34563456@gmail.com>
2025-04-24 17:24:43 +0600

    theme store added

--- components/ThemeShowcase.vue
+++ components/ThemeShowcase.vue
@@ -9,17 +9,50 @@
         </div>
 
 
-        <a href="#"
-            class="flex flex-col items-center bg-card border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-                src="/images/demo1.jpg" alt="">
-            <div class="flex flex-col justify-between p-4 leading-normal">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-primary dark:text-white">Noteworthy technology
-                    acquisitions 2021</h5>
-                <p class="mb-3 font-normal text-primary dark:text-gray-400">Here are the biggest enterprise technology
-                    acquisitions of 2021 so far, in reverse chronological order.</p>
+        <div class="flex">
+            <div class="max-w-sm bg-card border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <img class="rounded-t-lg" src="/images/demo1.jpg" alt="" />
+                </a>
+                <div class="p-5">
+                    <a href="#">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-primary dark:text-white">Noteworthy
+                            technology acquisitions 2021</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-primary dark:text-gray-400">Here are the biggest enterprise
+                        technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <a href="#"
+                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-primary rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Read more
+                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        </svg>
+                    </a>
+                </div>
             </div>
-        </a>
+
+            <div class="py-16 ">
+                <div class="grid row-gap-8 sm:grid-cols-3">
+                    <div class="text-center px-4">
+                        <h6 class="text-5xl font-bold text-primary">144K</h6>
+                        <p class="font-bold text-primary">Downloads</p>
+                    </div>
+                    <div class="text-center px-4">
+                        <h6 class="text-5xl font-bold text-primary">12.9K</h6>
+                        <p class="font-bold text-primary">Subscribers</p>
+                    </div>
+                    <div class="text-center px-4">
+                        <h6 class="text-5xl font-bold text-primary">27.3K</h6>
+                        <p class="font-bold text-primary">Users</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
 
 
     </div>
